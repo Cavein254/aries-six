e94539ae45b256f0896b2c54f00df3b0ae344483
johnmuthua <caveomuthama@gmail.com>
2023-08-26 21:13:46 +0300

    Added error handling

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -4,10 +4,6 @@ import { NextResponse } from 'next/server';
 const bcrypt = require('bcryptjs');
 
 export async function POST(request: Request) {
-  console.log('YYYYYYYYYYYYYYYYYYYYY');
-  console.log('YYYYYYYYYYYYYYYYYYYYY');
-  console.log('YYYYYYYYYYYYYYYYYYYYY');
-  console.log('YYYYYYYYYYYYYYYYYYYYY');
   const res = await request.json();
   const { name, email, password } = res;
   console.log(res);
@@ -22,28 +18,11 @@ export async function POST(request: Request) {
     });
     return NextResponse.json(user);
   } catch (err) {
-    return NextResponse.json(err);
+    if (err.code === 'P2002') {
+      return NextResponse.json(
+        'The email or username is already registed try another',
+        { status: 401 }
+      );
+    }
   }
-  // if (req.method === 'POST') {
-  //   console.log('Body body body');
-  //   console.log(req.body);
-  //   const { name, email, password } = req.body;
-  //   console.log(req.body);
-
-  //   try {
-  //     const hash = await bcrypt.hashSync(password, 6);
-  // const user = await prisma.user.create({
-  //   data: {
-  //     name: name,
-  //     email: email,
-  //     password: hash,
-  //   },
-  // });
-  // return res.json(user);
-  //   } catch (err) {
-  //     return NextResponse.json(err);
-  //   }
-  // } else {
-  //   return NextResponse.json('Method not allowed');
-  // }
 }
